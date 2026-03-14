8d039ccfc73ed0b5a3085bdcd8befe910737dc38
deadstar <deadstar1st@gmail.com>
2026-03-14 13:49:16 +0700

    untracked files on main: 34ce1e9 sửa lại khựng bottomsheet

---
+++ Services/LanguageService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using MauiAppMain.Resources.Localization;
+
+namespace MauiAppMain.Services
+{
+    internal class LanguageService
+    {
+        const string ReferencesName = "App_language";
+
+        public static void SetLanguage(string languageCode)
+        {
+            var culture = new CultureInfo(languageCode);
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            AppResource.Culture = culture;
+
+            // SAVE language
+            Preferences.Set(ReferencesName, languageCode);
+            // reload UI
+            Application.Current.MainPage = new AppShell();
+        }
+
+        public static void LoadSavedLanguage()
+        {
+            var languageCode = Preferences.Get(ReferencesName, "en");
+
+            var culture = new CultureInfo(languageCode);
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            AppResource.Culture = culture;
+        }
+    }
+}
